71a4b3e44354057fd937666e44cc1e68317fdd5c
Prerit Tyagi <tyagiprerit36@gmail.com>
2026-04-06 11:26:12 +0530

    Update page.tsx

--- app/upload-xray/page.tsx
+++ app/upload-xray/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Image from "next/image";
 import { useMemo, useState } from "react";
 import AppFooter from "@/components/AppFooter";
 import ImageUploader from "@/components/ImageUploader";
